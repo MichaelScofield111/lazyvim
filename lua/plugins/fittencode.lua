b51f9b3ed544a486c71c4911e245107c6727407b
MichaelScofield111 <141600921+MichaelScofield111@users.noreply.github.com>
2025-03-20 00:11:37 +0800

    updara fittencode.lua key map

--- lua/plugins/fittencode.lua
+++ lua/plugins/fittencode.lua
@@ -1,27 +1,26 @@
 return {
-  'luozhiya/fittencode.nvim',
-  config = function ()
-  require('fittencode').setup({
-     keymaps = {
-    inline = {
-      ['<S-CR>'] = 'accept_all_suggestions',
-      ['<C-e>'] = 'accept_line',
-      ['<C-i>'] = 'accept_word',
-      ['<C-u>'] = 'revoke_line',
-      ['<C-n>'] = 'revoke_word',
-      ['<C-\\>'] = 'triggering_completion',
-    },
-    chat = {
-      ['q'] = 'close',
-      ['[c'] = 'goto_previous_conversation',
-      [']c'] = 'goto_next_conversation',
-      ['c'] = 'copy_conversation',
-      ['C'] = 'copy_all_conversations',
-      ['d'] = 'delete_conversation',
-      ['D'] = 'delete_all_conversations',
-    }
-  },
-  })
+  "luozhiya/fittencode.nvim",
+  config = function()
+    require("fittencode").setup({
+      keymaps = {
+        inline = {
+          ["<C-Tab>"] = "accept_all_suggestions",
+          ["<C-e>"] = "accept_line",
+          ["<C-i>"] = "accept_word",
+          ["<C-u>"] = "revoke_line",
+          ["<C-n>"] = "revoke_word",
+          ["<C-\\>"] = "triggering_completion",
+        },
+        chat = {
+          ["q"] = "close",
+          ["[c"] = "goto_previous_conversation",
+          ["]c"] = "goto_next_conversation",
+          ["c"] = "copy_conversation",
+          ["C"] = "copy_all_conversations",
+          ["d"] = "delete_conversation",
+          ["D"] = "delete_all_conversations",
+        },
+      },
+    })
   end,
 }
-
