3cfe639c599e02f7db55b6d88af83598df34108c
MichaelScofield111 <141600921+MichaelScofield111@users.noreply.github.com>
2025-03-21 00:16:33 +0800

    fix nvim-lsp

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("lazy").setup({
+  spec = {
+    { import = "plugins.extras" }, -- 额外加载 extras 目录
+    { import = "plugins" }, -- 加载 plugins 目录
+  },
+})
